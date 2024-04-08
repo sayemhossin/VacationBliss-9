6e25910e37301c754839d571899a3fdcfc998b6b
sayemhossin <sayemhossin22@gmail.com>
2024-04-08 17:48:59 +0600

    added footer

--- src/Root/Root.jsx
+++ src/Root/Root.jsx
@@ -1,11 +1,16 @@
 import {Outlet} from 'react-router-dom'
 import Navbar from '../Components/Navbar/Navbar';
+import Footer from '../Pages/Footer/Footer';
 const Root = () => {
     return (
         <div>
            <div className='container mx-auto'>
             <Navbar></Navbar>
            <Outlet></Outlet>
+         
+           </div>
+           <div className='mt-14'>
+           <Footer></Footer>
            </div>
         </div>
     );
