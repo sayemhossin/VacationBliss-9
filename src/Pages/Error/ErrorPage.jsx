04cbd1ddb43eab6852a90203634c39cfb277893b
sayemhossin <sayemhossin22@gmail.com>
2024-04-17 02:17:16 +0600

    added more helmet

--- src/Pages/Error/ErrorPage.jsx
+++ src/Pages/Error/ErrorPage.jsx
@@ -1,8 +1,12 @@
+import { Helmet } from "react-helmet-async";
 import { Link } from "react-router-dom";
 
 const ErrorPage = () => {
     return (
         <section className="flex items-center h-full p-16   text-gray-100 dark:text-gray-800">
+			<Helmet>
+            <title>Page Not Found</title>
+          </Helmet>
 	<div className="container flex flex-col items-center justify-center px-5 mx-auto my-8">
 		<div className="max-w-md text-center">
 			<h2 className="mb-8 font-extrabold text-9xl text-gray-600 dark:text-gray-400">
